350236040921deab45e715696fee4b26d1c27df7
sanjaynain754 <sanjaynain754@gmail.com>
2026-07-11 10:09:40 +0530

    Format code in shield.go for consistency

--- but-p2p/shield.go
+++ but-p2p/shield.go
@@ -1,32 +1,32 @@
 package main
 
 import (
-	"log"
-	"sync"
-	"time"
+    "log"
+    "sync"
+    "time"
 )
 
 type MirrorShield struct {
-	mu        sync.RWMutex
-	blacklist map[string]time.Time
+    mu        sync.RWMutex
+    blacklist map[string]time.Time
 }
 
 func NewMirrorShield() *MirrorShield {
-	return &MirrorShield{
-		blacklist: make(map[string]time.Time),
-	}
+    return &MirrorShield{
+        blacklist: make(map[string]time.Time),
+    }
 }
 
 func (ms *MirrorShield) Block(addr string) {
-	ms.mu.Lock()
-	defer ms.mu.Unlock()
-	ms.blacklist[addr] = time.Now()
-	log.Printf("[!] Mirror Shield: Blocked %s", addr)
+    ms.mu.Lock()
+    defer ms.mu.Unlock()
+    ms.blacklist[addr] = time.Now()
+    log.Printf("[!] Mirror Shield: Blocked %s", addr)
 }
 
 func (ms *MirrorShield) IsBlocked(addr string) bool {
-	ms.mu.RLock()
-	defer ms.mu.RUnlock()
-	_, blocked := ms.blacklist[addr]
-	return blocked
+    ms.mu.RLock()
+    defer ms.mu.RUnlock()
+    _, blocked := ms.blacklist[addr]
+    return blocked
 }
