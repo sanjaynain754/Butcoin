89472912c6a198e9bad61c18b114d527a433d804
sanjaynain754 <sanjaynain754@gmail.com>
2026-07-11 15:57:18 +0530

    Add Uint8List import to vault_guard.dart

--- but-wallet/lib/utils/vault_guard.dart
+++ but-wallet/lib/utils/vault_guard.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'dart:convert';
 import 'dart:math';
+import 'dart:typed_data';
 
 class VaultGuard {
   // Android Keystore / iOS Keychain storage
